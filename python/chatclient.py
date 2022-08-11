7eaef424702ba3e72748d8d6d677cb96d8ad5a20
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-11 18:54:07 -0300

    Update

--- python/chatclient.py
+++ python/chatclient.py
@@ -1,79 +1,25 @@
-#from socket  import *
-import sys
-#import pickle
+"""The Python implementation of the GRPC helloworld.Greeter client."""
+
 from __future__ import print_function
+
 import logging
 
-#import threading
 import grpc
 import chatserver_pb2
 import chatserver_pb2_grpc
-import const #- addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
 
-"""
-class RecvHandler(threading.Thread):
-  def __init__(self, sock):
-    threading.Thread.__init__(self)
-    self.client_socket = sock
 
-  def run(self):
-    while True:
-        #print('Client receiving handler is ready.')
-        (conn, addr) = self.client_socket.accept() # accepts connection from server
-        #print('Server connected to me.')
-        marshaled_msg_pack = conn.recv(1024)   # receive data from server
-        msg_pack = pickle.loads(marshaled_msg_pack) # unmarshal message pack
-        print("MESSAGE: " + msg_pack[0] + " - FROM: " + msg_pack[1])
-        conn.send(pickle.dumps("ACK")) # simply send the server an Ack to confirm
-        conn.close()
-    return # We need a condition for graceful termination
-"""
-"""
-me = str(sys.argv[1]) # User's name (as registered in the registry. E.g., Alice, Bob, ...)
-#client_sock = socket(AF_INET, SOCK_STREAM) # socket for server to connect to this client
-my_ip = const.registry[me][0]   # If using a proper naming service, client should know its
-my_port = const.registry[me][1] # addresses (which it would register in the ns)
-#client_sock.bind((my_ip, my_port))
-#client_sock.listen(5)
-#
-# Put receiving thread to run
-#recv_handler = RecvHandler(client_sock)
-#recv_handler.start()
-#
-# Handle interactive loop
-"""
 def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
     with grpc.insecure_channel('172.17.0.2:50051') as channel:
-        stub = chatserver_pb2_grpc.ChatServiceStub(channel)
-        response = stub.SendMessage(chatserver_pb2.ForwardMessage(dest_ip='172.17.0.3'))
-    print("Send message: " + response.message)
+        stub = chatserver_pb2_grpc.MessageStub(channel)
+        response = stub.SendMessage(chatserver_pb2.MessageData(name='you'))
+    print("Greeter client received: " + response.message)
 
-"""while True:
-    server_sock = socket(AF_INET, SOCK_STREAM) # socket to connect to server
-    dest = input("ENTER DESTINATION: ")
-    msg = input("ENTER MESSAGE: ")
-    #
-    # Connect to server
-    try:
-        server_sock.connect((const.CHAT_SERVER_HOST, const.CHAT_SERVER_PORT))
-    except:
-        print("Server is down. Exiting...")
-        exit(1)
-    #
-    # Send message and wait for confirmation
-    msg_pack = (msg, dest, me)
-    marshaled_msg_pack = pickle.dumps(msg_pack)
-    server_sock.send(marshaled_msg_pack)
-    marshaled_reply = server_sock.recv(1024)
-    reply = pickle.loads(marshaled_reply)
-    if reply != "ACK":
-        print("Error: Server did not accept the message (dest does not exist?)")
-    else:
-        #print("Received Ack from server")
-        pass
-    server_sock.close()
-"""
 
 if __name__ == '__main__':
     logging.basicConfig()
     run()
+    
--- python/chatserver.py
+++ python/chatserver.py
@@ -1,74 +1,22 @@
-#from socket  import *
-#import pickle
-import logging
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
 from concurrent import futures
 import logging
+
 import grpc
 import chatserver_pb2
 import chatserver_pb2_grpc
-import const #- addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
-"""
-server_sock = socket(AF_INET, SOCK_STREAM) # socket for clients to connect to this server
-server_sock.bind((const.CHAT_SERVER_HOST, const.CHAT_SERVER_PORT))
-server_sock.listen(5) # may change if too many clients
-"""
-class ChatService(chatserver_pb2_grpc.ChatServiceServicer):
+
+
+class Message(chatserver_pb2_grpc.MessageServicer):
 
     def SendMessage(self, request, context):
-        return chatserver_pb2.ForwardMessage(message_data='Message forward for' % request.name)
+        return chatserver_pb2.ForwardMessage(message='Hello, %s!' % request.name)
 
-print("Chat Server is ready...")
-"""
-while True:
-    #
-    # Get a message from a sender client
-    (conn, addr) = server_sock.accept()  # returns new socket and addr. client
-
-    #print("Chat Server: client is connected from address " + str(addr))
-
-    marshaled_msg_pack = conn.recv(1024)   # receive data from client
-    msg_pack = pickle.loads(marshaled_msg_pack)
-    msg = msg_pack[0]
-    dest = msg_pack[1]
-    src = msg_pack[2]
-    print("RELAYING MSG: " + msg + " - FROM: " + src + " - TO: " + dest) # just print the message and destination
-    #
-    # Check that the destination exists
-    try:
-        dest_addr = const.registry[dest] # get address of destination in the registry
-    except:
-        conn.send(pickle.dumps("NACK")) # to do: send a proper error code
-    else:
-        #print("Server: sending Ack to " + src)
-        conn.send(pickle.dumps("ACK")) # send ACK to client
-    conn.close() # close the connection
-    #
-    # Forward the message to the recipient client
-    client_sock = socket(AF_INET, SOCK_STREAM) # socket to connect to clients
-    dest_ip = dest_addr[0]
-    dest_port = dest_addr[1]
-    try:
-        #print("Server: Trying to connect to (" + dest_ip + "," + str(dest_port) + ")")
-        client_sock.connect((dest_ip, dest_port))
-    except:
-        print ("Error: Destination client is down")
-        continue
-    msg_pack = (msg, src)
-    marshaled_msg_pack = pickle.dumps(msg_pack)
-    client_sock.send(marshaled_msg_pack)
-    marshaled_reply = client_sock.recv(1024)
-    reply = pickle.loads(marshaled_reply)
-    if reply != "ACK":
-        print("Error: Destination client did not receive message properly")
-    else:
-        #print("Server: Received Ack from client")
-        pass
-    client_sock.close()
-"""
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    chatserver_pb2_grpc.add_ChatServiceServicer_to_server(ChatService(), server)
+    chatserver_pb2_grpc.add_MessageServicer_to_server(Message(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()

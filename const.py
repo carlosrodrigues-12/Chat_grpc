964d174b9730d908ac38fec18cb6b31e3e7e0e7b
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-11 11:44:57 -0300

    Update const

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "10.128.0.3"
+CHAT_SERVER_HOST = "172.17.0.2"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
+registry = {"Alice":("172.17.0.3",5002), "Bob":("172.17.0.4",5002)}

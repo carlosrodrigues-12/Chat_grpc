d33fea9a4621f2744263438d58301b6a6c3cc0eb
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-13 08:46:37 -0300

    Comment

--- python/const.py
+++ python/const.py
@@ -4,4 +4,4 @@ CHAT_SERVER_PORT = 5001
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
 registry = {"Alice":("172.17.0.3",5002), "Bob":("172.17.0.4",5002)}
  
-## Comment
+## Comment 

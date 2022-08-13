c9123f339c7cc5f864e852e9b058179ec34fab25
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-13 09:56:23 -0300

    Server

--- python/chatserver.py
+++ python/chatserver.py
@@ -12,20 +12,26 @@ class Message(chatserver_pb2_grpc.MessageServicer):
 
     def SendMessage(self, request, context):
         # Print name
-        print(request.name)
-        print(request)
-        print(self)
         return chatserver_pb2.ForwardMessage(message='Hello, %s!' % request.name)
 
 
 def serve():
+    print('Init')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    print('Server -> ' % server)
     chatserver_pb2_grpc.add_MessageServicer_to_server(Message(), server)
+    print('Function add server')
     server.add_insecure_port('[::]:50051')
+    print('Add server - IP/Port')
     server.start()
+    print('Start server')
     server.wait_for_termination()
+    print('Server terminated')
 
 
 if __name__ == '__main__':
+    print('Main - Init')
     logging.basicConfig()
+    print('Logging - Basic Config')
     serve()
+    print('Call serve')
